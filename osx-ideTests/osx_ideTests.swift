049f2b8cd561b1ffabaeb996f504d1092c15a532
Jack Trefon <jack@trefon.com>
2025-08-25 16:29:21 +0400

    Initial Commit

---
+++ osx-ideTests/osx_ideTests.swift
@@ -0,0 +1,17 @@
+//
+//  osx_ideTests.swift
+//  osx-ideTests
+//
+//  Created by Jack Trefon on 25/08/2025.
+//
+
+import Testing
+@testable import osx_ide
+
+struct osx_ideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ osx-ideUITests/osx_ideUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  osx_ideUITestsLaunchTests.swift
+//  osx-ideUITests
+//
+//  Created by Jack Trefon on 25/08/2025.
+//
+
+import XCTest
+
+final class osx_ideUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
