136361bc0eab184672d82e142a2b8b950909ec26
Jack Trefon <jack@trefon.com>
2025-08-25 16:29:21 +0400

    Initial Commit

---
+++ osx-ide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  osx-ide
+//
+//  Created by Jack Trefon on 25/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ osx-ide/osx_ideApp.swift
@@ -0,0 +1,17 @@
+//
+//  osx_ideApp.swift
+//  osx-ide
+//
+//  Created by Jack Trefon on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct osx_ideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
